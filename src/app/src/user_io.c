23644bd253992c4e65ff76f24b5ac5f4992a117f
sengulhamza <sengulhamza@icloud.com>
2022-07-29 18:59:42 +0300

    software timers added

--- src/app/src/user_io.c
+++ src/app/src/user_io.c
@@ -2,11 +2,14 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/timers.h"
 #include "app/app_tasks.h"
 #include "app/user_io.h"
 #include "drv/io.h"
+#include "drv/softTimer.h"
 
 static TaskHandle_t s_user_io_task;
+static TimerHandle_t s_user_io_timer[TIMER_COUNT];
 
 static void user_io_task(void *params)
 {
@@ -23,8 +26,32 @@ static void user_io_task(void *params)
     }
 }
 
+static void user_io_timer_cb(TimerHandle_t xTimer)
+{
+    uint32_t timer_id = (uint32_t)pvTimerGetTimerID(xTimer);
+    timer_id = timer_id;
+    SoftTimer_ISR();
+}
+
+void user_io_timer_init(uint8_t timer_no)
+{
+    s_user_io_timer[timer_no] = xTimerCreate(
+                                    "s_user_io_timer",
+                                    pdMS_TO_TICKS(1),
+                                    pdTRUE,                                          // Auto-reload
+                                    (void *)(int)timer_no,                             // Timer ID
+                                    user_io_timer_cb);                                // Callback function
+    xTimerStart(s_user_io_timer[timer_no], portMAX_DELAY);
+}
+
 void user_io_process_start(void)
 {
+    user_io_timer_init(TIMER_A);
+    user_io_timer_init(TIMER_B);
+    SoftTimer_Init();
+    SoftTimer_SetTimer(TIMER_A, 10);
+    SoftTimer_SetTimer(TIMER_B, 20);
+
     xTaskCreate(user_io_task,
                 APP_USER_IO_TASK_NAME,
                 APP_USER_IO_TASK_STACK,
---
+++ src/drv/inc/drv/softTimer.h
@@ -0,0 +1,21 @@
+#ifndef SOFT_TIMER_H
+#define SOFT_TIMER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef enum {
+    TIMER_A,
+    TIMER_B,
+    TIMER_MAX
+} timer_num_e;
+
+#define TIMER_COUNT         TIMER_MAX       // istenilen soft counter sayisi
+
+void SoftTimer_Init(void);
+void SoftTimer_ISR(void);
+void SoftTimer_SetTimer(uint8_t TimerNo, uint32_t SetTimeMs);
+bool SoftTimer_GetTimerStatus(uint8_t TimerNo);
+void SoftTimer_ResetTimer(uint8_t TimerNo);
+
+#endif
