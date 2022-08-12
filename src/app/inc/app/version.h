a24824ddaec2c1b0985386d39864dc817745c873
sengulhamza <sengulhamza@icloud.com>
2022-08-12 15:12:21 +0300

    version changed

--- src/app/inc/app/version.h
+++ src/app/inc/app/version.h
@@ -1,9 +1,9 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define     PROJECT_VER        "1.1.2"
-#define     VER_MAJOR          1
+#define     PROJECT_VER        "0.1.1"
+#define     VER_MAJOR          0
 #define     VER_MINOR          1
-#define     VER_DEBUG          2
+#define     VER_DEBUG          1
 
 #endif /* _VERSION_H_ */
